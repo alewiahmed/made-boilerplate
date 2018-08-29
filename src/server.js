5e7c4ed82b9f4bf4628dd1e95980109264d4812b
Alewi Ahmed <alewimail@gmail.com>
2018-08-30 00:03:53 +0300

    Added express-jwt middleware for authentication

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import jwt from 'express-jwt';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 import config from '../config';
 import connectMongo from './db';
 import schema from './graphql/schema';
 
-let { SERVER_PORT } = process.env;
+let { SERVER_PORT, JWT_SECRET } = process.env;
 
 const playground = {
   settings: {
@@ -23,10 +24,21 @@ const start = async () => {
     mockEntireSchema: false,
     typeDefs: schema.typeDefs,
     resolvers: schema.resolvers,
-    context: {
-      ...models
+    context: ({ req }) => {
+      return {
+        ...models,
+        userInfo: req.user ? { _id: req.user.id, email: req.user.email } : null
+      };
     }
   });
+
+  app.use(
+    server.graphqlPath,
+    jwt({
+      secret: JWT_SECRET,
+      credentialsRequired: false
+    })
+  );
 
   server.applyMiddleware({ app });
 
