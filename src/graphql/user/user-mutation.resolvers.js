8e3738d46794730dad6eca3e6305b55f3c1b4f5e
Alewi Ahmed <alewimail@gmail.com>
2018-08-30 14:16:33 +0300

    Added login mutation

--- src/graphql/user/user-mutation.resolvers.js
+++ src/graphql/user/user-mutation.resolvers.js
@@ -3,9 +3,32 @@ import bcrypt from 'bcrypt';
 import { tokenize } from '../../lib';
 import baseResolver from '../baseResolver';
 import { RegistrationError } from '../errors';
-import { emailNotTakenResolver } from '../accessControl/auth';
+import {
+  emailNotTakenResolver,
+  isNotAuthenticatedResolver
+} from '../accessControl/auth';
 
 const userMutationResolvers = {
+  // Login user mutation
+  login: isNotAuthenticatedResolver.createResolver(
+    async (root, { email, password }, { User, userInfo }) => {
+      let user = await User.findOne({ email });
+      if (!user) {
+        throw new LoginError();
+      }
+
+      // validate password
+      return bcrypt.compare(password, user.password).then(res => {
+        if (res) {
+          // create jwt
+          user = tokenize(user);
+          userInfo = { _id: user.id, email: user.email };
+          return user;
+        }
+      });
+    }
+  ),
+  // Register user mutation
   register: emailNotTakenResolver.createResolver(
     async (root, { name, email, password }, { User, userInfo }) => {
       let hash = await bcrypt.hash(password, 8);
