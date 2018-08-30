822ec5f866bd0376339332ffebb9f17edd1e95eb
Alewi Ahmed <alewimail@gmail.com>
2018-08-30 12:23:53 +0300

    Added LoginError & AlreadyAuthenticatedError to the errors export

--- src/graphql/errors.js
+++ src/graphql/errors.js
@@ -8,3 +8,16 @@ export const UnknownError = createError('UnknownError', {
 export const AuthenticationRequiredError = createError('UnauthorizedError', {
   message: 'You must login to do that'
 });
+
+// email/password is incorrect when loging in
+export const LoginError = createError('LoginError', {
+  message: 'Incorrect email or password'
+});
+
+// user is already logged in
+export const AlreadyAuthenticatedError = createError(
+  'AlreadyAuthenticatedError',
+  {
+    message: 'You are already authenticated'
+  }
+);
