393c5682fdf4dd1619f61a23db9249e681be8b40
Alewi Ahmed <alewimail@gmail.com>
2018-08-31 14:39:39 +0300

    Created the deletePost admin resolver.

--- src/graphql/admin/admin-mutation.resolvers.js
+++ src/graphql/admin/admin-mutation.resolvers.js
@@ -1,5 +1,24 @@
-import isAdminResolver from '../accessControl/auth';
+import { TheUserDoesNotExistError } from '../errors';
+import { isAdminResolver } from '../accessControl/auth';
 
-const adminMutationResolvers = {};
+const adminMutationResolvers = {
+  deletePost: isAdminResolver.createResolver(
+    async (root, { postIds }, { Post }) => {
+      let deleteResult = [];
+      await Promise.all(
+        postIds.map(async postId => {
+          try {
+            await Post.findOneAndDelete({ _id: postId }).then(post => {
+              if (post) deleteResult.push(post);
+            });
+          } catch (err) {
+            return err;
+          }
+        })
+      );
+      return deleteResult;
+    }
+  )
+};
 
 export default adminMutationResolvers;
